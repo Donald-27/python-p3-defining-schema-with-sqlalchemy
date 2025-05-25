44ecdd15847ec74881c61cf8b9413efddbd81438
Donald Kiprop <kipropdonald27@gmail.com>
2025-05-25 12:19:09 +0300

    Define Student model and persist schema with SQLAlchemy

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
-
-from sqlalchemy_sandbox import Student
+from lib.sqlalchemy_sandbox import Student, Base  
 
 engine = create_engine('sqlite:///students.db')
 
 if __name__ == '__main__':
+    Base.metadata.create_all(engine) 
     import ipdb; ipdb.set_trace()
